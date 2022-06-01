f6121a9d362c678fba8df46463991aab3fd77472
Salomon Romano <salo_romano@yahoo.com>
2022-06-01 11:06:05 -0500

    Screen Fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Nav from '../components/Nav'
 
 export default function Home() {
@@ -17,14 +16,16 @@ export default function Home() {
           So, you want to travel to
         </h2>
         <h1 className="font-title text-[80px] uppercase text-white">Space</h1>
-        <p className="font-body leading-8 px-12 flex-grow">
+        <p className="font-body leading-8 flex-grow">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
           experience!
         </p>
-        <div className="h-[150px] w-[150px] bg-white flex items-center justify-center rounded-full mx-auto mt-[80px]">
-          <p className="font-title uppercase text-xl text-black">Explore</p>
+        <div className="h-[150px] w-[150px] bg-white grid place-content-center rounded-full mx-auto mt-20">
+          <p className="font-title uppercase text-xl text-black tracking-[1.25px]">
+            Explore
+          </p>
         </div>
       </main>
     </div>
